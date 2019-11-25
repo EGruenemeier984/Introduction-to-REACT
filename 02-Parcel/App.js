eb560e44b5334f94c15c5c78f8de181fc9921d0e
Ethan <egruenemeier984@west-mec.org>
2019-11-25 13:20:59 -0700

    Started to create functional react components
    Created s navbar components as well as a search component.

--- 02-Parcel/App.js
+++ 02-Parcel/App.js
@@ -1,3 +1,21 @@
-import React from 'React';
-import Navbar from './components/Navbar';
-export default () => (<Navbar />);
+import React from 'react'
+import Navbar,{Navitem} from './components/Navbar';
+
+/**
+ * CONTENT OF APP COMPONENT
+ */
+export default () => {
+    return(
+        <Navbar title="Mango Airlines"/>
+        // <Footer/>
+   )
+};
+
+
+export const Welcome = ({...props}) => {
+    return <>
+          <h1> Hello, {props.name} </h1>
+          <h1> {props.title}</h1>
+          </>
+  }
+{/* <Welcome name="session 2127219783" title="jksdjlkds" />,    */}
